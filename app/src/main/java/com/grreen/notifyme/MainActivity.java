b7b6e25e272269eed4a1ff2ac7a2fb42d1c86c55
Sergio Kulchytsky <serhiyx16@gmail.com>
2015-12-11 18:52:02 +0200

    Create Main Activity and layout

---
+++ app/src/main/java/com/grreen/notifyme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.grreen.notifyme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by serhi on 11-Dec-15.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
